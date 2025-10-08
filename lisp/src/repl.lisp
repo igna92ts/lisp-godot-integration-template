5e5517a5389d70baf19c4b23242766794269a4aa
ignacio torres <igna_torres92@hotmail.com>
2025-10-08 12:17:51 +0900

    Delete unused variable

--- lisp/src/repl.lisp
+++ lisp/src/repl.lisp
@@ -2,6 +2,6 @@
 
 (defun start-swank-repl (&key (port 4005))
   "Start Swank REPL on the given port in a separate thread."
-  (setf swank:*use-dedicated-output-stream* t)
   (swank:create-server :port port :dont-close t)
   0)
+
